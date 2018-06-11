bdd4909f22d38c844f77e91d5df1fccf1f19c0ed
Yadiel <ycabr035@fiu.edu>
2018-06-11 15:05:03 -0400

    added hand rotations

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 
-let hrPosition = 20;
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
 let minPosition = (min*360/60) + (sec*(360/60)/60);
 let secPosition = sec*360/60;
 
