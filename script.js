06ea98de7c3ac38a1763199bc902f2a06b6528b8
Yadiel <ycabr035@fiu.edu>
2018-06-11 15:05:51 -0400

    added hand rotations

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+function runTheClock() {
+  var date = new Date();
 
-var date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60) + (sec*(360/60)/60);
-let secPosition = sec*360/60;
+  let hrPosition = (hr*360/12)+(min*(360/60)/12);
+  let minPosition = (min*360/60) + (sec*(360/60)/60);
+  let secPosition = sec*360/60;
 
 
-HOURHAND.style.transform = "rotate(" + hrPosition +"deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  HOURHAND.style.transform = "rotate(" + hrPosition +"deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
